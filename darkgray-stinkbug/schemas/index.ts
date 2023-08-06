e4c55c27d736bb6de2d8e479ff840a8187b15e3c
Ashutosh Kumar <ak6590707@gmail.com>
2023-08-06 12:57:09 +0530

    creating sanity schema

--- darkgray-stinkbug/schemas/index.ts
+++ darkgray-stinkbug/schemas/index.ts
@@ -1,1 +1,72 @@
-export const schemaTypes = []
+// schema.js (Sanity schema)
+
+export default {
+  name: 'chatbot',
+  type: 'document',
+  title: 'Chatbot',
+  fields: [
+    {
+      name: 'steps',
+      type: 'array',
+      title: 'Steps',
+      of: [{ type: 'step' }],
+    },
+  ],
+};
+
+// Step type
+export const step = {
+  name: 'step',
+  type: 'object',
+  fields: [
+    {
+      name: 'id',
+      type: 'string',
+      title: 'ID',
+    },
+    {
+      name: 'message',
+      type: 'string',
+      title: 'Message',
+    },
+    {
+      name: 'user',
+      type: 'boolean',
+      title: 'User',
+    },
+    {
+      name: 'options',
+      type: 'array',
+      title: 'Options',
+      of: [{ type: 'option' }],
+    },
+    {
+      name: 'trigger',
+      type: 'string',
+      title: 'Trigger',
+    },
+  ],
+};
+
+// Option type
+export const option = {
+  name: 'option',
+  type: 'object',
+  fields: [
+    {
+      name: 'value',
+      type: 'string',
+      title: 'Value',
+    },
+    {
+      name: 'label',
+      type: 'string',
+      title: 'Label',
+    },
+    {
+      name: 'trigger',
+      type: 'string',
+      title: 'Trigger',
+    },
+  ],
+};
