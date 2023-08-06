4ead67675596060cf3bc4b3d1f6e919856ec5805
Ashutosh Kumar <ak6590707@gmail.com>
2023-08-06 13:46:50 +0530

    added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrKii-D2-XHFSqNQpz6fbDDbMzWPuEArc",
+  authDomain: "chatbot-a4c4e.firebaseapp.com",
+  projectId: "chatbot-a4c4e",
+  storageBucket: "chatbot-a4c4e.appspot.com",
+  messagingSenderId: "1060272548963",
+  appId: "1:1060272548963:web:3bcdbf85f4b2643ab99c56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
